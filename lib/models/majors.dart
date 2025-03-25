19be25dc1874f6c496dc2bb70bce6c5ac2b02c09
zzz276 <zzeni2125@gmail.com>
2025-03-25 12:27:46 +0700

    Major Detail Page (Part 1)
    + Majors Class

---
+++ lib/models/majors.dart
@@ -0,0 +1,15 @@
+class Majors {
+  Majors({
+    required this.name,
+    required this.faculty,
+    required this.foundedYear,
+    required this.overview,
+    required this.videoLink
+  });
+
+  String name;
+  String faculty;
+  String foundedYear;
+  String overview;
+  String videoLink;
+}
--- lib/screens/contents/major/major_detail.dart
+++ lib/screens/contents/major/major_detail.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
 class MajorDetail extends StatelessWidget {
-  const MajorDetail({super.key});
+  const MajorDetail({
+    required this.title,
+    required this.faculty,
+    required this.foundedYear,
+    required this.overview,
+    required this.videoLink,
+    super.key
+  });
+
+  final String title;
+  final String faculty;
+  final String foundedYear;
+  final String overview;
+  final String videoLink;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0), 
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+
+          icon: const Icon(Icons.chevron_left_outlined)
+        ),
+
+        title: Text(title),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.all(16.0), 
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
