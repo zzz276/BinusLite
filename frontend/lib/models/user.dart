df3d1f1e61e23dee20ef6cae80554ce1e2540d0a
zzz276 <zzeni2125@gmail.com>
2025-05-13 10:52:04 +0700

    Update project structures

---
+++ frontend/lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String displayName;
+  String username;
+  String email;
+  String password;
+
+  User({
+    required this.displayName,
+    required this.username,
+    required this.email,
+    required this.password
+  });
+}
--- lib/models/user.dart
+++
@@ -1,8 +0,0 @@
-class User {
-  String displayName;
-  String username;
-  String email;
-  String password;
-
-  User({required this.displayName, required this.username, required this.email, required this.password});
-}
